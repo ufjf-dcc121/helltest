f89152064edce36a1a30046cf93adc98f1987c16
Igor Knop <igorknop@gmail.com>
2020-09-30 08:36:40 -0300

    Altera diff

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ if [ "$output" == "$expected_output" ] ; then
   echo "Passo 2 [OK]: Passou: A saída é correta"
 else
   echo "Passo 2 [Falhou] O professor esperava: '$expected_output' mas você enviou: '$output'"
-  colordiff <(echo "'$expected_output'")  <(echo "'$output'")
+  diff <(echo "'$expected_output'")  <(echo "'$output'")
   exit 1
 fi
 
